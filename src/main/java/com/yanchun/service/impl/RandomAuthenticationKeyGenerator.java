96eae0f50812deda47c4b51e15601ac4a24dd340
quyanchun <qyc199237@163.com>
2019-08-11 10:43:39 +0800

    加入security

---
+++ src/main/java/com/yanchun/service/impl/RandomAuthenticationKeyGenerator.java
@@ -0,0 +1,14 @@
+package com.yanchun.service.impl;
+
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.AuthenticationKeyGenerator;
+
+import java.util.UUID;
+
+public class RandomAuthenticationKeyGenerator implements AuthenticationKeyGenerator {
+
+    @Override
+    public String extractKey(OAuth2Authentication authentication) {
+        return UUID.randomUUID().toString();
+    }
+}
