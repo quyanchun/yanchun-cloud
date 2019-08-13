d8bf107cb74b1161ab9993d1fff2601881a7b475
quyanchun <qyc199237@163.com>
2019-08-13 11:08:05 +0800

    提交项目

---
+++ src/main/java/com/yanchun/token/service/impl/Oauth2ClientImpl.java
@@ -0,0 +1,21 @@
+package com.yanchun.token.service.impl;
+
+import com.yanchun.token.service.Oauth2Client;
+
+import java.util.Map;
+
+/**
+ * @Author quyanchun
+ * @Date 2019/8/13
+ */
+public class Oauth2ClientImpl implements Oauth2Client {
+    @Override
+    public Map<String, Object> postAccessToken(Map<String, String> parameters) {
+        return null;
+    }
+
+    @Override
+    public void removeToken(String access_token) {
+
+    }
+}
